1f80a96a6c64fd05f99b68655c804b9904914537
Daniel Paulino <daniel-paulino@hotmail.com>
2022-09-11 15:37:18 -0300

    14- Added params in routes.

---
+++ src/pages/api/users/[id].tsx
@@ -0,0 +1,17 @@
+import {NextApiRequest, NextApiResponse} from "next";
+
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    console.log(request.query);
+    //como arquivo tem o id escrito, ele vai pegar o
+    // valor atribuido a ele na url: example:
+    // /user/12
+
+
+    const users = [
+        {id: 1, name:'Diego'},
+        {id: 2, name:'Rafaela'},
+        {id: 3, name:'Fela'},
+    ]
+
+    return response.json(users);
+}
