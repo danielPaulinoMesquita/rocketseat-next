fc3244afe61a779f5fcc34dd46a931fc2925618e
Daniel Paulino <daniel-paulino@hotmail.com>
2022-09-18 15:38:19 -0300

    17- Added verification login to see if user exists or not.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
 import { fauna } from '../../../services/fauna'
-import {Collection, Create} from 'faunadb';
+import {Casefold, Collection, Create, Exists, Get, If, Index, Match, Not} from 'faunadb';
 
 export const authOptions = {
     // Configure one or more authentication providers
@@ -22,11 +22,27 @@ export const authOptions = {
 
             try {
                 await fauna.query(
-                    Create(
-                        Collection('users'),
-                        {data: {email}}
-                    ))
-
+                    If(
+                        Not(
+                            Exists(
+                                Match(
+                                    Index('user_by_email'),
+                                    Casefold(email)
+                                )
+                            )
+                        ),
+                        Create(
+                            Collection('users'),
+                            {data: {email}}
+                        ),
+                        Get(
+                            Match(
+                                Index('user_by_email'),
+                                Casefold(email)
+                            )
+                        )
+                    ),
+                )
                 return true
             }catch (e) {
                 console.log(e);
