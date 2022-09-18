6b0c1ed8f0ee4a1b1103a44de83021335ac884a1
Daniel Paulino <daniel-paulino@hotmail.com>
2022-09-18 00:43:38 -0300

    15- Added Authentocation with Next-auth to use github login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import '../styles/global.scss';
 import {Header} from '../components/Header';
+import {SessionProvider} from "next-auth/react";
 
-function MyApp({ Component , pageProps }) {
-  return (
-      <>
-        <Header/>
-        <Component {...pageProps} />
-      </>
+function MyApp({ session, Component , pageProps }) {
+    return (
+        <>
+            <SessionProvider session={session}>
+                <Header/>
+                <Component {...pageProps} />
+            </SessionProvider>
+        </>
 
-  )
+    )
 }
 
 export default MyApp
